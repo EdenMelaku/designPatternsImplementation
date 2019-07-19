7f6963fa7146e7a390bd5b43fb416f9a6f9bdaa1
eden melaku <edenzzzmelaku@gmail.com>
2019-07-19 12:27:26 +0300

    builder changed

--- src/creationalPattern/BuilderPattern.java
+++ src/creationalPattern/BuilderPattern.java
@@ -4,39 +4,183 @@ i take team building  as an example to show bulider pattern
 
 package creationalPattern;
 
-abstract class Employee{
+import java.util.ArrayList;
 
-    private String ID;
-    private String name;
+interface HabshaDigis{
+public String name();
+public Packing packaging();
 
-    public String getID() {
-        return ID;
+
+}
+
+interface Packing{
+
+    String pack();
+}
+
+class Sehan implements Packing{
+
+    @Override
+    public String pack() {
+        return "sehan";
+    }
+}
+
+class Birchiko implements Packing{
+
+    @Override
+    public String pack() {
+        return "birchiko";
+    }
+}
+
+ class Beverage implements HabshaDigis{
+
+
+     @Override
+     public String name() {
+         return "bevrage";
+     }
+
+     @Override
+     public Packing packaging() {
+         return new Birchiko() ;
+     }
+ }
+
+ class Migib implements HabshaDigis{
+
+
+     @Override
+     public String name() {
+         return "migib";
+     }
+
+     @Override
+     public Packing packaging() {
+         return new Sehan();
+     }
+ }
+
+
+class Tela extends Beverage{
+    @Override
+    public String name() {
+        return "tela";
     }
 
-    public void setID(String ID) {
-        this.ID = ID;
+    @Override
+    public Packing packaging() {
+        return new Birchiko() ;
+    }
+}
+
+ class Tejj extends Beverage{
+     @Override
+     public String name() {
+         return "tejj";
+     }
+
+     @Override
+     public Packing packaging() {
+         return new Birchiko() ;
+     }
+
+}
+
+ class YetsomMigib extends Migib{
+
+    @Override
+    public String name() {
+        return "yetsom migib";
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public Packing packaging() {
+        return new Sehan();
+    }
+}
+
+ class YefisikMigib extends Migib{
+
+    @Override
+    public String name() {
+        return "yefisik migib";
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public Packing packaging() {
+        return new Sehan();
+    }
+
+}
+
+class Bufie {
+    ArrayList<BufieMansat> list=new ArrayList<>();
+
+    void addFood(BufieMansat digis ){
+        list.add(digis);
+
+    }
+    void showFood(){
+       for(BufieMansat l : list){
+
+           System.out.println("migib type = "+l.getM().name());
+           System.out.println("migib package type = "+l.getM().packaging());
+           System.out.println("bevrage type = "+l.getB().name());
+           System.out.println("bevrage package type = "+l.getB().packaging());
+
+
+       }
+    }
+}
+
+class BufieMansat{
+
+   private Migib m;
+   private Beverage b;
+  BufieMansat yetsom(){
+      BufieMansat yet=new BufieMansat();
+      yet.setM(new YetsomMigib());
+      yet.setB(new Tejj());
+      return yet;
+  }
+  BufieMansat yefisik() {
+      BufieMansat yef=new BufieMansat();
+      yef.setM(new YefisikMigib());
+      yef.setB(new Tela());
+      return yef;
+  }
+
+    public Migib getM() {
+        return m;
+    }
+
+    public void setM(Migib m) {
+        this.m = m;
+    }
+
+    public Beverage getB() {
+        return b;
+    }
+
+    public void setB(Beverage b) {
+        this.b = b;
     }
 }
 
 
-
-
-
-
-
-
-
-
-
 public class BuilderPattern {
+
+
+    public static void main(String ... args ){
+
+    BufieMansat bufieMansat=new BufieMansat();
+    Bufie b=new Bufie();
+            b.addFood(bufieMansat.yetsom());
+    System.out.println("yetsom migboch");
+    b.showFood();
+    }
 
 }
 
